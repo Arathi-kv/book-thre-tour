28eb05ff699bb36499867c6a7927bb5c0ae7eec8
Arathi-k-v <arathi@signroots.com>
2026-05-23 11:09:57 +0530

    conatct updated

--- app/js/main.js
+++ app/js/main.js
@@ -284,3 +284,27 @@ window.addEventListener('scroll', function () {
     flyAb.classList.remove('hide');
   }
 });
+document.addEventListener("DOMContentLoaded", function () {
+  new Swiper(".populer-activities", {
+    loop: true,
+
+    slidesPerView: 3,
+    spaceBetween: 20,
+
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints: {
+      320: { slidesPerView: 1 },
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 3 },
+    },
+  });
+});
